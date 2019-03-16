5f13188e3e02c7cf9646514c6480a60d7596b628
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-15 19:05:36 -0600

    Added command line args parsing with yargs

--- notes-app/src/app.js
+++ notes-app/src/app.js
@@ -1,5 +1,43 @@
+import chalk from "chalk";
+import yargs from "yargs";
 import { getNotes } from "./notes";
 
-const notes = getNotes();
+yargs
+  .version("1.0.0")
+  .command({
+    command: "add",
+    describe: "Add a new note",
+    builder: {
+      title: {
+        describe: "Note title",
+        demandOption: true,
+        type: "string"
+      },
+      body: {
+        describe: "Note body",
+        demandOption: true,
+        type: "string"
+      }
+    },
+    handler: argv => {
+      console.log(`Title: ${argv.title}`);
+      console.log(`Body: ${argv.body}`);
+    }
+  })
+  .command({
+    command: "remove",
+    describe: "Remove a new note",
+    handler: () => console.log("removing a note")
+  })
+  .command({
+    command: "list",
+    describe: "List all notes",
+    handler: () => console.log("list of all notes")
+  })
+  .command({
+    command: "read",
+    describe: "Read a specific new note",
+    handler: () => console.log("reading a note")
+  });
 
-console.log(notes);
+yargs.parse();
