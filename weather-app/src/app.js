d7b57c86d059f0ac0f67464367722d71b099c7e3
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-16 23:19:40 -0600

    Added ghetto command line parameter for loc

--- weather-app/src/app.js
+++ weather-app/src/app.js
@@ -1,7 +1,14 @@
 import { geocode } from './utils/geocode';
 import { forecast } from './utils/forecast';
 
-geocode('Los Angelos', (error, geocodeData) => {
+const location = process.argv.slice(2)[0];
+
+if (!location) {
+  console.log('command line argument missing: City');
+  process.exit(0);
+}
+
+geocode(location, (error, geocodeData) => {
   if (error) {
     return console.error(error);
   }
