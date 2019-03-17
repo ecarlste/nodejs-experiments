a4c3e7082585c503c7f50ce1f43ccc49122d7eaa
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-17 13:05:11 -0600

    Tied form submit into weather api calls

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,20 @@
+/* eslint-disable no-undef */
 console.log('Client side javascript wished it had chalk.js!!!');
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  fetch(`http://localhost:3000/weather?address=${search.value}`).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data.location);
+        console.log(data.forecast);
+      }
+    });
+  });
+});
