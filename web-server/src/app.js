6bd4db58fddd9809015e0da6f4461c57b7cf050d
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-17 09:26:27 -0600

    Playing around with handlebars partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,30 @@
 import path from 'path';
 import express from 'express';
+import hbs from 'hbs';
 
 const app = express();
 const publicDir = path.join(__dirname, '../public');
+const viewsDir = path.join(__dirname, '../templates/views');
+const partialsDir = path.join(__dirname, '../templates/partials');
 
 app.set('view engine', 'hbs');
+app.set('views', viewsDir);
+hbs.registerPartials(partialsDir);
+
 app.use(express.static(publicDir));
 
 app.get('', (req, res) => {
   res.render('index', { title: 'Weather App', name: 'Erik S. Carlsten' });
+});
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About', name: 'Erik S. Carlsten' });
+});
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    name: 'Erik S. Carlsten',
+    helpText: 'Come and get your love...'
+  });
 });
 app.get('/weather', (req, res) => {
   res.send('Your weather!');
