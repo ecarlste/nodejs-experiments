323f32ad45ea5edd1640420cd355a8dc994f9ed4
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-17 13:28:27 -0600

    Wired form return data into display w/ styles

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -4,16 +4,22 @@ console.log('Client side javascript wished it had chalk.js!!!');
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 
+const locationName = document.querySelector('#location-name');
+const forecastInfo = document.querySelector('#forecast-info');
+
 weatherForm.addEventListener('submit', e => {
   e.preventDefault();
 
+  forecastInfo.textContent = 'Loading...';
+  locationName.textContent = '';
+
   fetch(`http://localhost:3000/weather?address=${search.value}`).then(response => {
-    response.json().then(data => {
-      if (data.error) {
-        console.log(data.error);
+    response.json().then(({ error, forecast, location }) => {
+      if (error) {
+        forecastInfo.textContent = error;
       } else {
-        console.log(data.location);
-        console.log(data.forecast);
+        locationName.textContent = location;
+        forecastInfo.textContent = forecast;
       }
     });
   });
